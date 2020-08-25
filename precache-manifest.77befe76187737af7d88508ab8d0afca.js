fb575fe41f95b2f2ea1c6aabc95d6f255a1b0bf3
Ian <41586905+ianbaxter@users.noreply.github.com>
2020-08-25 17:20:57 +0100

    Updates

---
+++ precache-manifest.77befe76187737af7d88508ab8d0afca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a358f86b4408f802a042f41505f3325",
+    "url": "/poetical/index.html"
+  },
+  {
+    "revision": "46190f238d95ee778a55",
+    "url": "/poetical/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "860890b761dd0ea8bee1",
+    "url": "/poetical/static/css/main.3631e4e2.chunk.css"
+  },
+  {
+    "revision": "46190f238d95ee778a55",
+    "url": "/poetical/static/js/2.3fbc98d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/poetical/static/js/2.3fbc98d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "860890b761dd0ea8bee1",
+    "url": "/poetical/static/js/main.c7c513d9.chunk.js"
+  },
+  {
+    "revision": "fd5198a205be0d7fa664",
+    "url": "/poetical/static/js/runtime-main.2b64be6e.js"
+  }
+]);
